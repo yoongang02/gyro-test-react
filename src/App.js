aa018e638d99a707274f7c2ebee940cdf3dfaa32
yeongseokim <1903020@ewhain.net>
2022-08-15 09:13:38 +0900

    css없이

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useRef } from 'react'
+import charImg from './img/char.png'
+import stopImg from './img/btn-stop.png'
+import clockImg from './img/clock.png'
 
 function App() {
+  const [gamma, setGamma] = useState(0);
+  const [position, setPosition] = useState(0);
+  const [charRatio, setCharRatio] = useState(10);
+
+  const clientWidth = window.innerWidth;
+  const charSize = clientWidth / charRatio;
+  const maxPx = clientWidth / 2 - charSize / 2;
+  const minPx = -clientWidth / 2 + charSize / 2;
+  const rateConstant = 1.5 * clientWidth / 180;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='삭제예정'>
+        <div>clientWidth: {clientWidth}</div>
+        <div>gamma: {Math.round(gamma)}</div>
+        <div>px: {Math.round(position)}</div>
+        <div>maxPx: {maxPx}</div>
+        <div>minPx: {minPx}</div>
+      </div>
+      <div id='char-container'>
+        <img
+          id='char-img'
+          style={{
+            width: `${charSize}px`, position: 'relative', left: `${position}px`
+          }}
+          src={charImg} />
+      </div>
     </div>
   );
 }
